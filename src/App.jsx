1176159f27ebaef21df99978340e0a0f5639015a
lilgionate <140750280+lilgionate@users.noreply.github.com>
2024-03-11 18:32:33 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
+import Testimonials from "./components/testimonials/Testimonials";
+
+const App = () => {
+    return (
+        <>
+        <Header/>
+
+        <main className="main">
+            <Home/>
+            <About/>
+            <Skills/>
+            <Services/>
+            <Qualification/>
+            <Testimonials/>
+        </main>
+        </>
+    )
+}
+
+export default App 
---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,25 @@
+import Image1 from "../../assets/testimonial1.jpeg";
+import Image2 from "../../assets/testimonial2.jpeg";
+import Image3 from "../../assets/testimonial3.jpeg";
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "John Doe",
+        description: " A really good job, all aspects of the project were followed step by step and with good results."
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: "Chris Doe",
+        description: " A really good job, all aspects of the project were followed step by step and with good results."
+    },
+    {
+        id: 3,
+        image: Image3,
+        title: "Kelvin Doe",
+        description: " A really good job, all aspects of the project were followed step by step and with good results."
+    },
+];
+
