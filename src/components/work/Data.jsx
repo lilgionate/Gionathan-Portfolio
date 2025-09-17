a184e842cbd5668e01038943f1ca548ef358b4ed
Gionathan Vargas <gionathanvargas@Mac.lan>
2025-09-17 19:20:20 -0400

    added images

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,15 +1,15 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
-import Work6 from "../../assets/work2.jpg";
+import Work1 from "../../assets/project-1.png";
+import Work2 from "../../assets/project-2.png";
+import Work3 from "../../assets/project-3.png";
+import Work4 from "../../assets/demo-1.png";
+import Work5 from "../../assets/demo-2.png";
+import Work6 from "../../assets/demo-3.png";
 
 export const projectsData = [
     {
         id: 1,
         image: Work1,
-        title: "Web design",
+        title: "Stripe Billing & Subscription Portal",
         category: "Demos",
         demoUrl: "https://young-lake-53776-5ca5885c8205.herokuapp.com/",
         githubUrl: "https://github.com/wchoi888/virtual_bookshelf.git",
@@ -17,7 +17,7 @@ export const projectsData = [
     {
         id: 2,
         image: Work2,
-        title: "App movil",
+        title: "Trello Style Kanban Board",
         category: "Integrations",
         demoUrl: "https://deplo-b4f2f1dae76e.herokuapp.com/",
         githubUrl: "https://github.com/lilgionate/MVC-Tech-Blog.git",
@@ -25,25 +25,25 @@ export const projectsData = [
     {
         id: 3,
         image: Work3,
-        title: "Brand design",
+        title: "Business Analytics Dashboard",
         category: "Dashboards",
     },
     {
         id: 4,
         image: Work4,
-        title: "App movil",
+        title: "Salesforce CRM",
         category: "Demos",
     },
     {
         id: 5,
         image: Work5,
-        title: "Web design",
+        title: "Calendly Scheduling",
         category: "Integrations",
     },
     {
         id: 6,
         image: Work6,
-        title: "Brand design",
+        title: "Zendesk",
         category: "Dashboards",
     },
 ];
